0be0e1e8ac39cb9e3eb7755ac75c44a919e665ae
Chandhu Bhumireddy <89249579+bchandhu@users.noreply.github.com>
2024-01-02 15:50:37 +0000

    final changes

--- app.rb
+++ app.rb
@@ -2,26 +2,9 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  erb(:homepage)require "sinatra"
-require "sinatra/reloader"
-
-get("/") do
   erb(:homepage)
 end
 
-get("/dice/:number_of_dice/:how_many_sides") do
-  @num_dice = params.fetch("number_of_dice").to_i
-  @sides = params.fetch("how_many_sides").to_i
-  @rolls = []
-
-  @num_dice.times do
-    die = rand(1..@sides)
-    @rolls.push(die)
-  end
-
-  erb(:flexible)
-end
-end
 get("/dice/:number_of_dice/:how_many_sides") do
 
   @num_dice = params.fetch("number_of_dice").to_i
